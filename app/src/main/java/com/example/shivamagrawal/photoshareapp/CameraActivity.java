ccc60388857c312bf8c78fc484d0542529cdf4fb
Shivam Agrawal <shivamagrawal@Shivams-MacBook-Air.local>
2016-07-22 17:21:17 -0400

    idek but i think i made it better

--- app/src/main/java/com/example/shivamagrawal/photoshareapp/CameraActivity.java
+++ app/src/main/java/com/example/shivamagrawal/photoshareapp/CameraActivity.java
@@ -1,6 +1,8 @@
 package com.example.shivamagrawal.photoshareapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+
 import java.io.IOException;
 
 import android.hardware.Camera;
@@ -22,16 +24,16 @@ import android.graphics.Matrix;
 
 import java.io.ByteArrayOutputStream;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.Request;
-import com.android.volley.AuthFailureError;
+
+import com.example.shivamagrawal.photoshareapp.Objects.Server;
 
 @SuppressWarnings("deprecation")
 public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    // TODO: allow zoom, tap-to-focus, and filters
 
     private Camera mCamera;
 
@@ -42,12 +44,15 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     private ImageView captureImage;
     private ImageView switchCameras;
 
+    private Context context;
+
     private int currrentCamID = Camera.CameraInfo.CAMERA_FACING_BACK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        context = this;
 
         surfaceView = (SurfaceView) findViewById(R.id.camera_preview_surfaceview);
 
@@ -80,18 +85,10 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                 switchCameras();
             }
         });
-
-        // Make Full Screen
-        findViewById(R.id.camera_container_layout).setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     private void openCamera() {
+        // Camera should be null
         try {
             mCamera = Camera.open(currrentCamID);
 
@@ -107,7 +104,6 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
             parameters.setPictureSize(size.width, size.height);
 
             if (parameters.getFocusMode().equals("auto")) {
-                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
                 parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
                 mCamera.setParameters(parameters);
             }
@@ -117,8 +113,8 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     private void finishCamera() {
-        mCamera.stopPreview();
         if (mCamera != null) {
+            mCamera.stopPreview();
             mCamera.release();
             mCamera = null;
         }
@@ -126,27 +122,24 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     private void switchCameras() {
-        mCamera.stopPreview();
-        mCamera.release();
-        if (currrentCamID == Camera.CameraInfo.CAMERA_FACING_BACK) {
-            currrentCamID = Camera.CameraInfo.CAMERA_FACING_FRONT;
-        } else {
-            currrentCamID = Camera.CameraInfo.CAMERA_FACING_BACK;
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
         }
+        if (currrentCamID == Camera.CameraInfo.CAMERA_FACING_BACK)
+            currrentCamID = Camera.CameraInfo.CAMERA_FACING_FRONT;
+        else currrentCamID = Camera.CameraInfo.CAMERA_FACING_BACK;
         openCamera();
     }
 
 
     private void capture() {
         mCamera.takePicture(null, null, null, new Camera.PictureCallback() {
-
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
-                Log.d("PIC", "CAPTURED");
-
                 if (data != null)
                     new UploadPhoto().execute(data);
-
                 mCamera.startPreview();
             }
         });
@@ -174,7 +167,24 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
             bm.compress(Bitmap.CompressFormat.PNG, 100, baos); // quality doesn't matter b/c PNG is lossless
             byte[] imageBytes = baos.toByteArray();
             String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-            Log.d("ENCODED", encodedImage);
+
+            Map<String, String> parameters = new HashMap<String, String>();
+            parameters.put("image", encodedImage);
+            StringRequest sr = Server.POST(parameters, Server.uploadPhotoURL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("RES", response.isEmpty()?"EMPTY RESPONSE": response);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("ERR", "ERR");
+                        }
+                    }
+            );
+            Server.makeRequest(context, sr);
 
             //sendImage(encodedImage);
             //MediaStore.Images.Media.insertImage(getContentResolver(), bm, "LOL", "LOLOL"); // store in image gallery
@@ -182,74 +192,6 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
             return true;
         }
     }
-
-    private void reqGet(final String image) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest sr = new StringRequest(Request.Method.POST,
-                "http://10.0.0.11:3000/api/upload",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("INTERNET", response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("INTERNET", error.getMessage());
-            }
-        }){
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("image", image);
-                return params;
-            }
-
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("Content-Type","application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        queue.add(sr);
-    }
-
-    private void sendImage(String image) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("image", image);
-        //queue.add(Server.POST(params));
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // TODO: better buttons, image rotation etc.
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        openCamera();
-    }
-
-    // HELPER METHODS
 
     private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
         final double ASPECT_TOLERANCE = 0.1;
@@ -283,44 +225,30 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         return optimalSize;
     }
 
-    public void setCameraDisplayOrientation() {
-        Camera.CameraInfo info = new Camera.CameraInfo();
-        Camera.getCameraInfo(currrentCamID, info);
-        int rotation = this.getWindowManager().getDefaultDisplay().getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
-        }
-        int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360; // compensate the mirror
-        } else { // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
-        mCamera.setDisplayOrientation(result);
-    }
-
-
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        Log.d("Surface Created", "");
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.d("Surface Destroyed", "");
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        openCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Make Full Screen
+        findViewById(R.id.camera_container_layout).setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
@@ -329,6 +257,7 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         if (mCamera != null) {
             mCamera.stopPreview();
             mCamera.release();
+            mCamera = null;
         }
     }
 }
